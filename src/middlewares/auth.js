92ed8dad8de3e7d92796b1967a34de263d20bf0e
samsul-rijal <samsulr99@gmail.com>
2022-02-10 14:50:01 +0700

    update

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,25 @@
 // import package here
+const jwt = require("jsonwebtoken")
 
 exports.auth = (req, res, next) => {
   // code here
+  const authHeader = req.header("Authorization")
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if(!token) {
+    return res.status(401).send({ message: "Access denied"})
+  }
+
+  try {
+
+    const SECRET_KEY = 'BebasApajasilahkan'
+
+    const verified = jwt.verify(token, SECRET_KEY)
+    req.user = verified
+    next()
+
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: 'Invalid token'})
+  }
 };
